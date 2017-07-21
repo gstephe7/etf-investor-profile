bc12948f29b8d995668ef36d96a33446231a3b90
Greg <gstephe7@gmail.com>
2017-07-21 09:03:41 -0400

    alt tag added to ally ad

--- src/components/AllyTextAd.js
+++ src/components/AllyTextAd.js
@@ -7,7 +7,7 @@ export default class AllyTextAd extends Component {
       <div className="container">
         <div className="row">
           <div className="col-xs-12">
-            <h4><a className="text-center" href="http://www.kqzyfj.com/click-8391792-12928606" target="_top">Open an Investment Account</a></h4><img src="http://www.ftjcfx.com/image-8391792-12928606" width="1" height="1" border="0"/>
+            <h4><a className="text-center" href="http://www.kqzyfj.com/click-8391792-12928606" target="_top">Open an Investment Account</a></h4><img src="http://www.ftjcfx.com/image-8391792-12928606" alt="" width="1" height="1" border="0"/>
           </div>
         </div>
       </div>
